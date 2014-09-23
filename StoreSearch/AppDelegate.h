39914aa4572243b6fd03b9c89cc6bfcdb5e8203c
Alan <Alan@aaabbb.local>
2014-09-23 18:44:15 +0800

    create project

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by Alan on 14-9-23.
+//  Copyright (c) 2014年 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
